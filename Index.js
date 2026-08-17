829da9f0d47d84c047f4d6f22ad84451a8786e42
Yeldah94 <younasrasmussen@gmail.com>
2026-08-17 12:59:10 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -6,25 +6,26 @@ gopeed.events.onResolve(async function (ctx) {
     throw new Error("Mangler Real-Debrid API Token i indstillingerne.");
   }
 
+  // Sender URL-encoded body i stedet for JSON
+  const bodyParams = "link=" + encodeURIComponent(rawUrl);
+
   const res = await gopeed.fetch("https://api.real-debrid.com/rest/1.0/unrestrict/link", {
     method: "POST",
     headers: {
       "Authorization": "Bearer " + token,
       "Content-Type": "application/x-www-form-urlencoded"
     },
-    data: {
-      link: rawUrl
-    }
+    data: bodyParams
   });
 
   if (res.status !== 200) {
-    throw new Error("Real-Debrid API svarede med HTTP " + res.status);
+    throw new Error("Real-Debrid API fejl: HTTP " + res.status);
   }
 
   const data = typeof res.data === "string" ? JSON.parse(res.data) : res.data;
 
   if (data.error) {
-    throw new Error("Real-Debrid fejl: " + data.error);
+    throw new Error("RD Fejl: " + data.error);
   }
 
   if (data.download) {
@@ -36,7 +37,6 @@ gopeed.events.onResolve(async function (ctx) {
       }
     };
   } else {
-    throw new Error("Kunne ikke modtage download-link fra Real-Debrid.");
-  
-}
+    throw new Error("Ingen download-link modtaget fra Real-Debrid.");
+  }
 });
